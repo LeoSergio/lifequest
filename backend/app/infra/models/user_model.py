b50275384a31dc375f2a8bf094ca3ed155973428
LeoSergio <leandrooos222@gmail.com>
2026-07-27 15:37:24 -0300

    fix: add updated_at to UserModel to resolve sqlalchemy attribute error

--- backend/app/infra/models/user_model.py
+++ backend/app/infra/models/user_model.py
@@ -31,3 +31,4 @@ class UserModel(Base):
     
     # Metadados
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
